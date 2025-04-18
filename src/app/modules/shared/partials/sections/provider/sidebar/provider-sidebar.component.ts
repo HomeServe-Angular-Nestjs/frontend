db0ef4f22afcd531a0c4028d03b5119e182d47b0
sajid189210 <sajid189210@gmail.com>
2025-04-18 09:40:54 +0530

    created provider side bar and its components

---
+++ src/app/modules/shared/partials/sections/provider/sidebar/provider-sidebar.component.ts
@@ -0,0 +1,100 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-provider-sidebar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './provider-sidebar.component.html',
+  // styleUrl: './provider-sidebar.component.scss',
+})
+export class ProviderSidebarComponent {
+  menuItems = [
+    {
+      title: 'primary navigation',
+      subItems: [
+        {
+          name: 'Dashboard',
+          icon: 'fas fa-tachometer-alt',
+          route: '',
+          active: true
+        },
+        {
+          name: 'Bookings',
+          icon: 'fas fa-calendar-check',
+          route: '',
+          active: false
+        }
+      ]
+    },
+    {
+      title: 'analytics',
+      subItems: [
+        {
+          name: 'Performance',
+          icon: 'fas fa-chart-line',
+          route: '',
+          active: false
+        },
+        {
+          name: 'Revenue',
+          icon: 'fas fa-dollar-sign',
+          route: '',
+          active: false
+        },
+        {
+          name: 'Area Analytics',
+          icon: 'fas fa-map-marked-alt',
+          route: '',
+          active: false
+        }
+      ]
+    },
+    {
+      title: 'account management',
+      subItems: [
+        {
+          name: 'Profile',
+          icon: 'fas fa-user-circle',
+          route: 'provider/profiles',
+          active: false
+        },
+        {
+          name: 'messages',
+          icon: 'fas fa-envelope',
+          route: '',
+          active: false
+        },
+        {
+          name: 'Notifications',
+          icon: 'fas fa-bell',
+          route: '',
+          active: false
+        }
+      ]
+    },
+    {
+      title: 'Business Tools',
+      subItems: [
+        {
+          name: 'Reviews',
+          icon: 'fas fa-star',
+          route: '',
+          active: false
+        },
+        {
+          name: 'Earnings',
+          icon: 'fas fa-wallet',
+          route: '',
+          active: false
+        },
+        {
+          name: 'Settings',
+          icon: 'fas fa-cog',
+          route: '',
+          active: false
+        }
+      ]
+    }
+  ]
+}
