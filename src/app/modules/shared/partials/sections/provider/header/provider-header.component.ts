bcc1c4500c8b4efa15b1199b727f050cc7c4c306
sajid189210 <sajid189210@gmail.com>
2025-04-18 09:41:29 +0530

    created provider header and its components

---
+++ src/app/modules/shared/partials/sections/provider/header/provider-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-provider-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './provider-header.component.html',
+  // styleUrl: './provider-header.component.scss',
+})
+export class ProviderHeaderComponent {}
