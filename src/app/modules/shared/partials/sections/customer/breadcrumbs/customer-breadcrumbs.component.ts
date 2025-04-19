fcd0931ccc88fa03a753c22ec808908afc6d2994
sajid189210 <sajid189210@gmail.com>
2025-04-19 15:20:30 +0530

    feat(Customer): added brumbcrumbs

---
+++ src/app/modules/shared/partials/sections/customer/breadcrumbs/customer-breadcrumbs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-customer-breadcrumbs',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './customer-breadcrumbs.component.html',
+})
+export class CustomerBreadcrumbsComponent {}
