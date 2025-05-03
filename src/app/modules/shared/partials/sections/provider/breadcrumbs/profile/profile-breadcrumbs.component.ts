0494668b1e9b14fc5e589c2992a03f7748663371
sajid189210 <sajid189210@gmail.com>
2025-05-03 19:34:23 +0530

    refactor: profiles, update-> breadcrumbs selection changes

--- src/app/modules/shared/partials/sections/provider/breadcrumbs/profile/profile-breadcrumbs.component.ts
+++ src/app/modules/shared/partials/sections/provider/breadcrumbs/profile/profile-breadcrumbs.component.ts
@@ -1,26 +1,27 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
-import { RouterLink } from "@angular/router";
+import { Component, inject, OnInit } from "@angular/core";
+import { ActivatedRoute, Router, RouterLink } from "@angular/router";
 
 @Component({
     selector: 'app-profile-breadcrumbs',
     templateUrl: './profile-breadcrumbs.component.html',
     imports: [CommonModule, RouterLink]
 })
-export class ProfileBreadcrumbsComponent {
+export class ProfileBreadcrumbsComponent implements OnInit {
+    private _router = inject(Router);
 
     crumbs = [
         {
             name: 'Profile Overview',
             icon: 'fa-user-circle',
-            active: true,
+            active: false,
             route: 'overview'
         },
         {
             name: 'About Section',
             icon: 'fa-info-circle',
             active: false,
-            route: ''
+            route: 'about'
         },
         {
             name: 'Service Offered',
@@ -38,9 +39,18 @@ export class ProfileBreadcrumbsComponent {
             name: 'Gallery',
             icon: 'fa-images',
             active: false,
-            route: ''
+            route: 'gallery'
         },
     ];
+
+    ngOnInit() {
+        const segments = this._router.url.split('?')[0].split('/');
+        this.crumbs.forEach(item => {
+            if (item.route && segments.includes(item.route)) {
+                this.changeStatus(item.name);
+            }
+        });
+    }
 
     changeStatus(name: string): void {
         this.crumbs = this.crumbs.map((item) => ({
