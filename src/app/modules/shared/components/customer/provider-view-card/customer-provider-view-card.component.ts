77e6d40fd501a92fed82eb691bcb71f145d67880
sajid189210 <sajid189210@gmail.com>
2025-05-22 20:41:05 +0530

    feat(customer) refactor-> provider view card | update-> added fallback for undefined avatar

--- src/app/modules/shared/components/customer/provider-view-card/customer-provider-view-card.component.ts
+++ src/app/modules/shared/components/customer/provider-view-card/customer-provider-view-card.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IProvider } from '../../../../../core/models/user.model';
 import { Router } from '@angular/router';
+import { getColorFromChar } from '../../../../../core/utils/style.utils';
 
 @Component({
   selector: 'app-customer-provider-view-card',
@@ -13,59 +14,15 @@ export class CustomerProviderViewCardComponent {
   private router = inject(Router);
 
   @Input({ required: true }) providers!: IProvider[];
-
-  searchQuery = '';
-  certifiedOnly = true;
-  ratingFilter = 0;
-  priceFilter = 'all';
-  sortOption = 'rating';
-
-  filteredProviders: IProvider[] = [];
-
-  ngOnInit() {
-    // this.filterProviders();
-  }
-
-  filterProviders() {
-    this.filteredProviders = this.providers.filter((provider: IProvider) => {
-      // Search filter
-      const matchesSearch = provider.fullname
-        ? provider.fullname.toLowerCase().includes(this.searchQuery.toLowerCase())
-        : provider.username.toLowerCase().includes(this.searchQuery.toLowerCase());
-
-      // Certified filter
-      const matchesCertified = !this.certifiedOnly || provider.isCertified;
-
-      // Rating filter
-      // const matchesRating = provider.rating >= this.ratingFilter;
-
-      //     // Price filter
-      //     let matchesPrice = true;
-      //     if (this.priceFilter === '0-50') {
-      //       matchesPrice = provider.price <= 50;
-      //     } else if (this.priceFilter === '50-100') {
-      //       matchesPrice = provider.price > 50 && provider.price <= 100;
-      //     } else if (this.priceFilter === '100+') {
-      //       matchesPrice = provider.price > 100;
-      //     }
-
-      return matchesSearch && matchesCertified;
-    });
-    //   // this.sortProviders();
-  }
-
-  sortProviders() {
-    if (this.sortOption === 'rating') {
-      // this.filteredProviders.sort((a, b) => b.rating - a.rating);
-    } else if (this.sortOption === 'price') {
-      // this.filteredProviders.sort((a, b) => a.price - b.price);
-    } else if (this.sortOption === 'distance') {
-      // this.filteredProviders.sort((a, b) => a.distance - b.distance);
-    }
-  }
+  fallbackChar: string = '';
 
   toggleFavorite(provider: IProvider) {
     provider.isActive = !provider.isActive;
+  }
+
+  fallbackColor(text: string) {
+    this.fallbackChar = text.charAt(0).toUpperCase();
+    return getColorFromChar(text.charAt(0));
   }
 
   viewProvider(providerId: string) {
