61ba0cbdc4a118930eacaad807702442838e58f4
Muhammed Sajid S <sajid189210@gmail.com>
2025-06-20 09:04:43 +0530

    feat(customer provider details) updated the provider bio by displaying the details

--- src/app/modules/shared/components/customer/provider-details/profile-about/customer-provider-profile-about.component.ts
+++ src/app/modules/shared/components/customer/provider-details/profile-about/customer-provider-profile-about.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { filter, map, Observable, switchMap, tap } from 'rxjs';
+import { IProvider } from '../../../../../../core/models/user.model';
+import { ActivatedRoute } from '@angular/router';
+import { ProviderService } from '../../../../../../core/services/provider.service';
 
 @Component({
   selector: 'app-customer-provider-profile-about',
@@ -7,4 +11,16 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   templateUrl: './customer-provider-profile-about.component.html',
 })
-export class CustomerProviderProfileAboutComponent {}
+export class CustomerProviderProfileAboutComponent implements OnInit {
+  private readonly route = inject(ActivatedRoute);
+  private readonly providerService = inject(ProviderService);
+
+  provider$!: Observable<IProvider | null>;
+
+  ngOnInit(): void {
+    this.provider$ = this.route.parent!.paramMap.pipe(
+      map(param => param.get('id')),
+      switchMap(providerId => this.providerService.getOneProvider(providerId))
+    );
+  }
+}
