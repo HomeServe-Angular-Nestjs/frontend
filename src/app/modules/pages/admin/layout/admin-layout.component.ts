0091fdad529179c817baf913ad0053ce256703db
Muhammed Sajid S <sajid189210@gmail.com>
2025-07-08 20:03:04 +0530

    created subs components

---
+++ src/app/modules/pages/admin/layout/admin-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { AdminHeaderComponent } from '../../../shared/partials/sections/admin/header/admin-header.component';
+import { AdminSidebarComponent } from '../../../shared/partials/sections/admin/sidebar/admin-sidebar.component';
+
+@Component({
+  selector: 'app-admin-homepage',
+  templateUrl: './admin-layout.component.html',
+  imports: [CommonModule, AdminSidebarComponent, RouterOutlet, AdminHeaderComponent],
+})
+export class AdminHomepageComponent { }
---
+++ src/app/modules/shared/components/admin/subsciptions/admin-subscriptions.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'admin-subscriptions',
+    templateUrl: 'admin-subscriptions.component.html',
+    imports: []
+})
+export class AdminSubscriptionsComponent { }
