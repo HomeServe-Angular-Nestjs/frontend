c747ea9b194444862cd8b8f9b8e9fde81f14d805
Muhammed Sajid S <sajid189210@gmail.com>
2025-08-11 09:39:16 +0530

    feat(payment) made sure that a user cant initiate payment more than once at a time

--- src/app/core/services/payment.service.ts
+++ src/app/core/services/payment.service.ts
@@ -8,30 +8,13 @@ import { UType } from "../models/user.model";
 @Injectable()
 export class PaymentService {
     private _http = inject(HttpClient);
-
     private _apiUrl = API_ENV.payment;
 
-    private getErrorMessage(error: HttpErrorResponse): string {
-        return error?.error?.message || 'something went wrong';
-    }
-
     createRazorpayOrder(amount: number): Observable<RazorpayOrder> {
-        return this._http.post<RazorpayOrder>(`${this._apiUrl}/create_order`, { amount }).pipe(
-            catchError((error: HttpErrorResponse) =>
-                throwError(() =>
-                    new Error(this.getErrorMessage(error)))
-            )
-        );
+        return this._http.post<RazorpayOrder>(`${this._apiUrl}/create_order`, { amount });
     }
 
     verifyPaymentSignature(verifyData: RazorpayPaymentResponse, orderData: RazorpayOrder, role: UType): Observable<IVerifiedPayment> {
-        return this._http.post<IVerifiedPayment>(`${this._apiUrl}/verify_signature`, { verifyData, orderData, role }).pipe(
-            catchError((error: HttpErrorResponse) =>
-                throwError(() =>
-                    new Error(this.getErrorMessage(error)))
-            )
-        );
+        return this._http.post<IVerifiedPayment>(`${this._apiUrl}/verify_signature`, { verifyData, orderData, role });
     }
-
-
 }
