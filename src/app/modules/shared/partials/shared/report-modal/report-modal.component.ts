4b3d2eb97e6ed3d3816f1e7c505eed0863ecd61a
Muhammed Sajid S <sajid189210@gmail.com>
2025-09-19 19:17:39 +0530

    feat(complaints): added report feature in the provider side

--- src/app/modules/shared/partials/shared/report-modal/report-modal.component.ts
+++ src/app/modules/shared/partials/shared/report-modal/report-modal.component.ts
@@ -1,20 +1,38 @@
-import { Component, EventEmitter, inject, Output } from "@angular/core";
+import { Component, EventEmitter, inject, OnDestroy, Output } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { ToastNotificationService } from "../../../../../core/services/public/toastr.service";
+import { MatDialog } from "@angular/material/dialog";
+import { ConfirmDialogComponent } from "../confirm-dialog-box/confirm-dialog.component";
+import { Subject, takeUntil } from "rxjs";
+import { IReportSubmit } from "../../../../../core/services/report.service";
 
 @Component({
     selector: 'app-report-modal',
     templateUrl: 'report-modal.component.html',
     imports: [FormsModule]
 })
-export class ReportModalComponent {
+export class ReportModalComponent implements OnDestroy {
     private readonly _toastr = inject(ToastNotificationService);
+    private readonly _dialog = inject(MatDialog);
+
+    private destroy$ = new Subject<void>();
 
     @Output() closeEvent = new EventEmitter();
-    @Output() submitEvent = new EventEmitter();
+    @Output() submitEvent = new EventEmitter<Omit<IReportSubmit, 'targetId'>>();
 
     option = '';
     text = '';
+
+    ngOnDestroy(): void {
+        this.destroy$.next();
+        this.destroy$.complete();
+    }
+
+    private _openConfirmationDialog(message: string, title: string) {
+        return this._dialog.open(ConfirmDialogComponent, {
+            data: { title, message },
+        });
+    }
 
     submitReport() {
         if (!this.option) {
@@ -27,10 +45,23 @@ export class ReportModalComponent {
             return;
         }
 
-        this.submitEvent.emit({ reason: this.option, description: this.text })
+        this._openConfirmationDialog(
+            'Are you sure you want to submit this report? Once submitted, our team will review it.',
+            'Confirm Report Submission')
+            .afterClosed()
+            .pipe(takeUntil(this.destroy$))
+            .subscribe((isConfirmed) => {
+                if (!isConfirmed) {
+                    this.closeModal();
+                    return;
+                }
+                this.submitEvent.emit({ reason: this.option, description: this.text })
+            });
     }
 
     closeModal() {
+        this.option = '';
+        this.text = '';
         this.closeEvent.emit('');
     }
 } 
