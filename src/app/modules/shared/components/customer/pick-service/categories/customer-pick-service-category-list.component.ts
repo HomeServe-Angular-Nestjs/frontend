6cb14e5280f10eee50279457e34a3016c2b6a4e5
Muhammed Sajid S <sajid189210@gmail.com>
2025-09-20 09:17:04 +0530

    fix(pick-a-service): changed from constructor to oninit and used combinelatest for better readability

--- src/app/modules/shared/components/customer/pick-service/categories/customer-pick-service-category-list.component.ts
+++ src/app/modules/shared/components/customer/pick-service/categories/customer-pick-service-category-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, effect, EventEmitter, inject, Input, OnInit, Output, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-customer-pick-service-category-list',
@@ -7,15 +9,35 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   templateUrl: './customer-pick-service-category-list.component.html',
 })
-export class CustomerPickServiceCategoryListComponent {
-  @Input({ required: true }) serviceCategories: { title: string, image: string }[] = [];
+export class CustomerPickServiceCategoryListComponent implements OnInit {
+  private readonly _route = inject(ActivatedRoute);
+
+  private destroy$ = new Subject<void>();
+
+  @Input({ required: true }) serviceCategories: { title: string, image: string, id: string }[] = [];
   @Output() categorySelectEvent = new EventEmitter<string>();
 
-  selectedCategory: string | null = null;
+  selectedCategoryTitle: string | null = null;
+  selectedCategoryId: string | null = null;
 
-  selectCategory(categoryTitle: string) {
-    this.selectedCategory = categoryTitle;
-    this.categorySelectEvent.emit(categoryTitle);
+  ngOnInit(): void {
+    this._route.queryParamMap
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(query => {
+
+      });
+
+    // effect(() => {
+    //   const categoryId = this.selectedCategory();
+    //   if (categoryId) { 
+
+    //   }
+    // })
+  }
+
+  selectCategory(selectedCategoryTitle: string) {
+    this.selectedCategoryTitle = selectedCategoryTitle;
+    this.categorySelectEvent.emit(selectedCategoryTitle);
   }
 }
 
