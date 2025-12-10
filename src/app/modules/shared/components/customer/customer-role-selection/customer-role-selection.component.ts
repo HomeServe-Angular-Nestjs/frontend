35a609b6677625fcbae280bc832e26dbac5da655
Muhammed Sajid S <sajid189210@gmail.com>
2025-12-10 10:08:06 +0530

    feat(landing-page): added a landing page -- closes #48

---
+++ src/app/modules/shared/components/customer/customer-role-selection/customer-role-selection.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-customer-role-selection',
+  imports: [RouterLink],
+  templateUrl: './customer-role-selection.component.html',
+})
+export class CustomerRoleSelectionComponent { }
--- src/app/modules/shared/components/customer/hero-section/customer-hero-section.component.ts
+++ src/app/modules/shared/components/customer/hero-section/customer-hero-section.component.ts
@@ -1,46 +1,38 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-customer-hero-section',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './customer-hero-section.component.html',
+  imports: [CommonModule, RouterLink]
 })
 export class CustomerHeroSectionComponent {
   currentSlide = 0;
   intervalId: any;
 
   images = [
-    {
-      url: 'asset/images/hero_image1.jpg',
-      alt: 'Home construction'
-    },
-    {
-      url: 'https://images.unsplash.com/photo-1605276374104-dee2a0ed3cd6',
-      alt: 'Home cleaning'
-    },
-    {
-      url: 'https://images.unsplash.com/photo-1600121848594-d8644e57abab',
-      alt: 'Home electrical'
-    }
+    { url: 'assets/images/hero_image1.jpg', alt: 'Home construction' },
+    { url: 'assets/images/hero_image2.jpg', alt: 'Cleaning service' },
+    { url: 'assets/images/hero_image3.jpg', alt: 'Electrical work' }
   ];
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.startCarousel();
   }
 
-  ngOnDestroy(): void {
+  ngOnDestroy() {
     clearInterval(this.intervalId);
   }
 
-  startCarousel(): void {
+  startCarousel() {
     this.intervalId = setInterval(() => {
       this.currentSlide = (this.currentSlide + 1) % this.images.length;
     }, 5000);
   }
 
-  selectSlide(index: number): void {
+  selectSlide(index: number) {
     this.currentSlide = index;
     clearInterval(this.intervalId);
     this.startCarousel();
