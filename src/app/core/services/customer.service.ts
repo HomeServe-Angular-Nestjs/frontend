7e880b5a3301f169469ad447cecee1e13a1ed813
Muhammed Sajid S <sajid189210@gmail.com>
2026-01-22 16:08:42 +0530

    fix: profile edit save and removed unnececary components

--- src/app/core/services/customer.service.ts
+++ src/app/core/services/customer.service.ts
@@ -1,59 +1,58 @@
-import { HttpClient, HttpErrorResponse, HttpParams } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { API_ENV } from "../../../environments/env";
 import { IChangePassword, ICustomer, ICustomerProfileData, IDisplayReviews } from "../models/user.model";
-import { catchError, Observable, throwError } from "rxjs";
+import { Observable } from "rxjs";
 import { IFilter } from "../models/filter.model";
 import { IResponse } from "../../modules/shared/models/response.model";
 import { ISubmitReview } from "../models/reviews.model";
-import { ICustomerSearchCategories } from "../models/category.model";
 
 @Injectable({ providedIn: 'root' })
 export class CustomerService {
-    private readonly _http = inject(HttpClient);
-    private readonly _apiUrl = API_ENV.customer;
+  private readonly _http = inject(HttpClient);
+  private readonly _apiUrl = API_ENV.customer;
 
-    getCustomers(filter: IFilter = {}): Observable<ICustomer[]> {
-        let params = new HttpParams();
+  getCustomers(filter: IFilter = {}): Observable<ICustomer[]> {
+    let params = new HttpParams();
 
-        Object.entries(filter).forEach(([key, value]) => {
-            if (value !== undefined && value !== null) {
-                params = params.set(key, value);
-            }
-        });
+    Object.entries(filter).forEach(([key, value]) => {
+      if (value !== undefined && value !== null) {
+        params = params.set(key, value);
+      }
+    });
 
-        return this._http.get<ICustomer[]>(`${this._apiUrl}/customers`, { params });
-    }
+    return this._http.get<ICustomer[]>(`${this._apiUrl}/customers`, { params });
+  }
 
-    fetchOneCustomer(): Observable<ICustomer> {
-        return this._http.get<ICustomer>(`${this._apiUrl}/customer`);
-    }
+  fetchOneCustomer(): Observable<ICustomer> {
+    return this._http.get<ICustomer>(`${this._apiUrl}/customer`);
+  }
 
-    partialUpdate(data: Partial<ICustomer>): Observable<ICustomer> {
-        return this._http.patch<ICustomer>(`${this._apiUrl}/partial_update`, data);
-    }
+  partialUpdate(data: Partial<ICustomer>): Observable<ICustomer> {
+    return this._http.patch<ICustomer>(`${this._apiUrl}/partial_update`, data);
+  }
 
-    updateAddToSaved(providerId: string): Observable<ICustomer> {
-        return this._http.patch<ICustomer>(`${this._apiUrl}/saved_providers`, { providerId });
-    }
+  updateAddToSaved(providerId: string): Observable<ICustomer> {
+    return this._http.patch<ICustomer>(`${this._apiUrl}/saved_providers`, { providerId });
+  }
 
-    updateProfile(profileData: ICustomerProfileData): Observable<IResponse<ICustomer>> {
-        return this._http.put<IResponse<ICustomer>>(`${this._apiUrl}/profile`, profileData);
-    }
+  updateProfile(profileData: ICustomerProfileData): Observable<IResponse<ICustomer>> {
+    return this._http.put<IResponse<ICustomer>>(`${this._apiUrl}/profile`, profileData);
+  }
 
-    changePassword(passwordData: IChangePassword): Observable<IResponse<ICustomer>> {
-        return this._http.patch<IResponse<ICustomer>>(`${this._apiUrl}/password`, passwordData);
-    }
+  changePassword(passwordData: IChangePassword): Observable<IResponse<ICustomer>> {
+    return this._http.patch<IResponse<ICustomer>>(`${this._apiUrl}/password`, passwordData);
+  }
 
-    changeAvatar(formData: FormData): Observable<IResponse<ICustomer>> {
-        return this._http.patch<IResponse<ICustomer>>(`${this._apiUrl}/avatar`, formData);
-    }
+  changeAvatar(formData: FormData): Observable<IResponse<ICustomer>> {
+    return this._http.patch<IResponse<ICustomer>>(`${this._apiUrl}/avatar`, formData);
+  }
 
-    submitReview(reviewData: ISubmitReview): Observable<IResponse<IDisplayReviews>> {
-        return this._http.post<IResponse<IDisplayReviews>>(`${this._apiUrl}/reviews`, reviewData);
-    }
+  submitReview(reviewData: ISubmitReview): Observable<IResponse<IDisplayReviews>> {
+    return this._http.post<IResponse<IDisplayReviews>>(`${this._apiUrl}/reviews`, reviewData);
+  }
 
-    getProviderGalleryImages(providerId: string): Observable<IResponse<string[]>> {
-        return this._http.get<IResponse<string[]>>(`${this._apiUrl}/gallery_images/${providerId}`);
-    }
+  getProviderGalleryImages(providerId: string): Observable<IResponse<string[]>> {
+    return this._http.get<IResponse<string[]>>(`${this._apiUrl}/gallery_images/${providerId}`);
+  }
 }
---
+++ src/app/modules/shared/components/customer/profile/profile-overview.component.ts
@@ -0,0 +1,221 @@
+import { CommonModule } from "@angular/common";
+import { Component, inject, OnDestroy, OnInit } from "@angular/core";
+import { map, Observable, of, Subject, takeUntil } from "rxjs";
+import { ICustomer } from "../../../../../core/models/user.model";
+import { Store } from "@ngrx/store";
+import { selectCustomer } from "../../../../../store/customer/customer.selector";
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { getValidationMessage } from "../../../../../core/utils/form-validation.utils";
+import { ToastNotificationService } from "../../../../../core/services/public/toastr.service";
+import { customerActions } from "../../../../../store/customer/customer.actions";
+import { REGEXP_ENV } from "../../../../../../environments/env";
+import { MapboxMapComponent } from "../../../partials/shared/map/map.component";
+import { LocationService } from "../../../../../core/services/public/location.service";
+
+@Component({
+  selector: 'app-customer-profile-overview',
+  templateUrl: './profile-overview.component.html',
+  imports: [CommonModule, ReactiveFormsModule, MapboxMapComponent],
+  providers: [LocationService]
+})
+export class CustomerProfileOverviewComponent implements OnInit, OnDestroy {
+  private readonly _toastr = inject(ToastNotificationService);
+  private readonly _locationService = inject(LocationService);
+  private readonly _fb = inject(FormBuilder);
+  private readonly _store = inject(Store);
+
+  private _destroy$ = new Subject<void>();
+  private _originalCustomerData!: string;
+
+  isEditing = false;
+  isLoading = true;
+
+  customer$: Observable<ICustomer | null> = this._store.select(selectCustomer);
+  googleLogin$: Observable<boolean> = of(false);
+  center: [number, number] = [76.9560, 8.5010];
+  showMap = false;
+
+  passwordRegex = REGEXP_ENV.password;
+  phoneRegex = REGEXP_ENV.phone;
+
+  profileForm: FormGroup = this._fb.group({
+    fullname: ['', Validators.required],
+    username: ['', Validators.required],
+    phone: ['', [Validators.required, Validators.pattern(this.phoneRegex)]],
+    email: ['', [Validators.required, Validators.email]],
+    address: ['', Validators.required],
+    coordinates: [[], Validators.required]
+  });
+
+  changePasswordForm: FormGroup = this._fb.group({
+    currentPassword: ['', [Validators.required, Validators.pattern(this.passwordRegex)]],
+    newPassword: ['', [Validators.required, Validators.pattern(this.passwordRegex)]],
+    confirmPassword: ['', [Validators.required, Validators.pattern(this.passwordRegex)]],
+  });
+
+  ngOnInit(): void {
+    this.customer$
+      .pipe(
+        takeUntil(this._destroy$)
+      ).subscribe(customer => {
+        if (customer) this.isLoading = false;
+
+        if (!customer?.address) this.showMap = true;
+
+        this.profileForm.patchValue({
+          fullname: customer?.fullname,
+          username: customer?.username,
+          email: customer?.email,
+          phone: customer?.phone,
+          coordinates: customer?.location?.coordinates,
+          address: customer?.address
+        });
+
+        this._originalCustomerData = JSON.stringify(this.profileForm.getRawValue());
+      });
+
+    this.googleLogin$ = this.customer$.pipe(
+      map(customer => !!customer?.googleId)
+    );
+  }
+
+  toggleEditMode() {
+    this.isEditing = !this.isEditing;
+    if (!this.isEditing) {
+      // If cancelling, reset data
+      if (this._originalCustomerData) {
+        this.profileForm.patchValue(JSON.parse(this._originalCustomerData));
+      }
+    }
+  }
+
+  onImageError(event: Event) {
+    (event.target as HTMLImageElement).src = 'assets/images/profile_placeholder.jpg';
+  }
+
+  saveProfile() {
+    this.profileForm.markAllAsTouched();
+    const controls = {
+      fullname: this.profileForm.get('fullname'),
+      username: this.profileForm.get('username'),
+      email: this.profileForm.get('email'),
+      phone: this.profileForm.get('phone'),
+      address: this.profileForm.get('address'),
+      coordinates: this.profileForm.get('coordinates')
+    };
+
+    const profileDataStr = this.profileForm.getRawValue();
+    const original = this._originalCustomerData ? JSON.parse(this._originalCustomerData) : {};
+
+    if (this.profileForm.valid) {
+      const profileData = {
+        fullname: controls.fullname?.value,
+        username: controls.username?.value,
+        phone: controls.phone?.value,
+        address: controls.address?.value,
+        coordinates: controls.coordinates?.value,
+      }
+
+      const hasChanged = JSON.stringify(profileDataStr) !== JSON.stringify(original);
+
+      if (!hasChanged) {
+        this.isEditing = false;
+        return;
+      }
+      this._store.dispatch(customerActions.updateProfile({ profileData }));
+      this.isEditing = false;
+      return;
+    }
+
+    const validate = (formGroup: FormGroup): string | null => {
+      for (const [key, control] of Object.entries(formGroup.controls)) {
+        if (control instanceof FormGroup) {
+          const nestedError = validate(control);
+          if (nestedError) return nestedError;
+        } else if (control instanceof FormControl) {
+          const message = getValidationMessage(control, key);
+          if (message) {
+            return message;
+          }
+        }
+      }
+      return null;
+    };
+
+    const firstError = validate(this.profileForm);
+
+    if (firstError) {
+      this._toastr.error(firstError);
+      return;
+    }
+  }
+
+  changePassword() {
+    this.changePasswordForm.markAllAsTouched();
+    const controls = {
+      currentPassword: this.changePasswordForm.get('currentPassword'),
+      newPassword: this.changePasswordForm.get('newPassword'),
+      confirmPassword: this.changePasswordForm.get('confirmPassword'),
+    }
+
+    if (this.changePasswordForm.valid) {
+      const passwordData = {
+        currentPassword: controls.currentPassword?.value,
+        newPassword: controls.newPassword?.value,
+        confirmPassword: controls.confirmPassword?.value
+      }
+
+      if (passwordData.currentPassword === passwordData.newPassword) {
+        this._toastr.error('New password should not be the same as current password.');
+        return;
+      }
+
+      if (passwordData.newPassword !== passwordData.confirmPassword) {
+        this._toastr.error('New password and Confirm password must match.');
+        return;
+      }
+
+      this._store.dispatch(customerActions.changePassword({ passwordData }));
+      this.changePasswordForm.reset(); // clear form
+
+    } else {
+      for (const [key, control] of Object.entries(controls)) {
+        const message = getValidationMessage(control, key);
+        if (message) {
+          this._toastr.error(message);
+          return;
+        }
+      }
+    }
+  }
+
+  changeAddress(newCenter: [number, number]) {
+    this.center = newCenter;
+    this._locationService.getAddressFromCoordinates(...newCenter).subscribe({
+      next: (data) => {
+        const selectedAddress = data.features[0]?.place_name;
+        this.profileForm.patchValue({
+          address: selectedAddress,
+          coordinates: this.center
+        });
+      },
+      error: (err) => {
+        this._toastr.error('Failed to get address.');
+      }
+    })
+  }
+
+  onAvatarChange(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const formData = new FormData();
+      formData.append('customerAvatar', file);
+      this._store.dispatch(customerActions.changeAvatar({ formData }));
+    }
+  }
+
+  ngOnDestroy(): void {
+    this._destroy$.next();
+    this._destroy$.complete();
+  }
+}
