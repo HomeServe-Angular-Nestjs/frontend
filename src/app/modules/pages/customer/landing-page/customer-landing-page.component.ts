d715f22389ae3b049a1760186eaad207681b90d7
Muhammed Sajid S <sajid189210@gmail.com>
2026-06-15 13:25:33 +0530

    enhanced landing page

--- src/app/modules/pages/customer/landing-page/customer-landing-page.component.ts
+++ src/app/modules/pages/customer/landing-page/customer-landing-page.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomerHeroSectionComponent } from '../../../shared/components/customer/hero-section/customer-hero-section.component';
 import { CustomerServiceOverviewComponent } from '../../../shared/components/customer/service-overview/customer-service-overview.component';
-import { authActions } from '../../../../store/auth/auth.actions';
-import { CustomerRoleSelectionComponent } from "../../../shared/components/customer/customer-role-selection/customer-role-selection.component";
+import { CustomerHowItWorksComponent } from '../../../shared/components/customer/how-it-works/customer-how-it-works.component';
 
 @Component({
   selector: 'app-customer-landing-page',
   standalone: true,
-  imports: [CommonModule, CustomerHeroSectionComponent, CustomerServiceOverviewComponent, CustomerRoleSelectionComponent],
+  imports: [CommonModule, CustomerHeroSectionComponent, CustomerHowItWorksComponent, CustomerServiceOverviewComponent],
   templateUrl: './customer-landing-page.component.html',
 })
 export class CustomerLandingPageComponent {
